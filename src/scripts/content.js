98db8876b3e217884dd5936e8605152715256775
Ilton Silveira <ton.garcia.jr@gmail.com>
2016-09-16 11:37:37 -0300

    content & settings versionated & quest_post moved up

---
+++ src/scripts/content.js
@@ -0,0 +1,142 @@
+content = {
+  brand: {
+    name: 'NITRO START',
+    icon: 'icon-nitrostart'
+  },
+  banner: {
+    main_value: 'Seu app a partir de R$ 5mil',
+    second_value: 'Triplique a chance de receber investimento',
+    video: 'https://www.youtube.com/embed/-Af7M1O3o0A',
+    bullets: [
+      'Seu app em apenas 2 semanas',
+      'Aumente suas chances de receber investimento',
+      'Seu app para validar com seus amigos e família'
+    ],
+    call_to_action: {
+      icon: 'ion-cash',
+      text: 'Shut up and take my money'
+    }
+  },
+
+  showroom: {
+    title: 'Um app bilionário para chamar de seu',
+    subtitle: 'Que app eu posso fazer com 5 mil em 2 semanas?',
+    call_to_action: {
+      icon: 'ion-ios-play',
+      text: 'começar meu app bilionário'
+    },
+    samples: [
+      {
+        color: 'black',
+        icon: 'icon-uber',
+        title: 'Perfeito para Uber das coisas',
+        call_to_action: {
+          icon: '',
+          color: '',
+          text: 'Quero meu uber'
+        },
+        description: '<strong>Ações padrão</strong>: cadastro e login condutor e passageiro <br> <strong>Ações customizadas</strong>: marcar ponto de encontro' +
+                      '<div class="small_margin_top small_margin_bottom line"><div class="left"></div><span>OU</span><div class="right"></div></div>' +
+                      '<strong>Ações customizadas</strong>: visualizar localização passageiro, condutor e vice-versa em tempo-real' +
+                      '<br><strong>Avaliado em</strong> <span class="teal-text">US$ 68 bilhões</span>'
+      },
+      {
+        color: 'green',
+        icon: 'ion-social-whatsapp-outline',
+        title: 'Perfeito para comunicação',
+        call_to_action: {
+          icon: '',
+          color: 'success',
+          text: 'Quero meu whatsapp'
+        },
+        description: '<strong>Ações padrão</strong>: cadastro e login (sem verificação via SMS) <br> <strong>Ação customizada</strong>: mensagens de texto instantâneas' +
+                     '<div class="small_margin_top small_margin_bottom line"><div class="left"></div><span>OU</span><div class="right"></div></div>' +
+                     '<strong>Ações customizadas</strong>: gerenciar grupos e contatos ou ainda envio de arquivos em tempo-real' +
+                     '<br><strong>Avaliado em</strong> <span class="teal-text">US$ 19 bilhões</span>'
+      },
+      {
+        color: 'blue',
+        icon: 'ion-social-twitter-outline',
+        title: 'Perfeito para redes sociais',
+        call_to_action: {
+          icon: '',
+          color: 'info',
+          text: 'Quero meu twitter'
+        },
+        description: '<strong>Ações padrão</strong>: cadastro e login (via facebook, gplus, twitter...) <br> <strong>Ação customizada</strong>: time-line simples (como no antigo twitter)' +
+                      '<div class="small_margin_top small_margin_bottom line"><div class="left"></div><span>OU</span><div class="right"></div></div>' +
+                      '<strong>Ações customizadas</strong>: gerenciar grupos e relacionamentos complexos como no GooglePlus' +
+                      '<br><strong>Avaliado em</strong> <span class="teal-text">US$ 10 bilhões</span>'
+      }
+    ]
+  },
+
+  partners: {
+    title: 'Instituições Parceiras',
+    description: 'As principais instituições de fomento e investimentos à startups estão conosco. Anjos do Brasil (maior rede de investidores anjos da américa latina), Criatec (maior fundo de investimentos do centro-oeste), ASTEPS (Segunda maior associação de startups do Brasil).',
+    brands: [
+      'p1.png',
+      'p2.png',
+      'p3.png'
+    ]
+  },
+
+  objections: {
+    title: 'Além de mais barato somos melhores',
+    subtitle: 'Somente com a NITRO START você alcançará seus mais altos objetivos, chega de alcançar metas mediocres',
+
+    // COMPLEMENTARY to the existing solution || UNIQUELY different
+    comparison: {
+      ourName: 'NITRO START',
+      competitorName: 'Outras fábricas de app',
+      points: [
+        {
+          competitor: 'Their disadvantage',
+          us: 'Our advantage'
+        },
+        {
+          competitor: 'Their disadvantage',
+          us: 'Our advantage'
+        },
+        {
+          competitor: 'Você recebe a melhor negociação no mesmo dia. Total comodidade e agilidade',
+          us: 'Você recebe a melhor negociação no mesmo dia. Total comodidade e agilidade'
+        },
+        {
+          competitor: 'Você recebe a melhor negociação no mesmo dia. Total comodidade e agilidade',
+          us: 'Você recebe a melhor negociação no mesmo dia. Total comodidade e agilidade'
+        }
+      ]
+    },
+
+    // 1. you understand and empathize with their objection
+    // 2. Offer new information (or discounts) that makes it easy for them to move forward
+    // 3. Get their commitment
+    friend_formula: {
+
+    }
+  },
+
+  guest_post: {
+    title: 'o que os investidores estão buscando',
+    call_to_action: {
+      icon: 'icon-nitrostart',
+      text: 'ME LEVE A ESTE ESTÁGIO'
+    },
+    guests: [
+      {
+        name: 'Cassio Spina',
+        role: 'Investidor Anjo e fundador da Anjos do Brasil',
+        summary: 'Somente após atingir o estágio de Protótipo você terá chances reais de conseguir investimento de anjos.' +
+                  ' Nenhum investidor profissional entra no estágio da ideia. ' +
+                  'A fase ideal para receber investimento é quando está dando os primeiros passos para vender seu produto.' +
+                  ' O investidor lhe ajudará com dinheiro e experiência.',
+        video: 'https://www.youtube.com/embed/e6J10EHbN6Q?list=PLjIhqe5hH3aRUXAGtrq2qj3qpmjHUtumW'
+      }
+    ]
+  },
+
+  social_proof: {
+    title: 'e com a palavra, os consumidores'
+  }
+};
---
+++ src/scripts/settings.js
@@ -0,0 +1,15 @@
+// Settings Variables
+domain_url = 'https://actionholding.com.br';
+
+// Server Connection
+firebase_app_id = 'nitrolanding-28802';
+fire_base_server = firebase_app_id + 'firebaseio.com';
+
+// Social Connections
+facebook_app_id = '1691426611079180';
+
+// View color palette
+visual_identity = {
+  btn: 'warning',
+  color: 'orange' // blue, green, light-green, orange, black
+};
